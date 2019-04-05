e9377de873bd9f602328d0fa23d317b24daf7e7b
haroldbarros <haroldbarros@gmail.com>
2019-04-05 13:19:33 -0300

    Development initial commit

--- CodeChallenge/Common/Constants.cs
+++ CodeChallenge/Common/Constants.cs
@@ -20,7 +20,7 @@ namespace CodeChallenge.Common
     public static class Constants
     {
         public const string API_URL = "https://api.themoviedb.org/3";
-        public const string API_KEY = "1f54bd990f1cdfb230adb312546d765d";
+        public const string API_KEY = "0a683ece7e6b9453cd7b586aee41c797";
         public const string DEFAULT_LANGUAGE = "";
         public const string DEFAULT_REGION = "";
     }
